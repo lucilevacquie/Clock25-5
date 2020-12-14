5a155eac0f8fb675ec221f0a9f67ac0ea7ecc9b1
Lucile Vacquie <lucilevacquie@luciles-mbp.mynet>
2020-12-14 14:57:07 +0000

    build the base with html and css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
 import React, {useState} from "react";
+import Button from '@material-ui/core/Button';
+import AddBoxTwoToneIcon from '@material-ui/icons/AddBoxTwoTone';
+import IndeterminateCheckBoxTwoToneIcon from '@material-ui/icons/IndeterminateCheckBoxTwoTone';
+import PlayCircleFilledTwoToneIcon from '@material-ui/icons/PlayCircleFilledTwoTone';
+import ReplayTwoToneIcon from '@material-ui/icons/ReplayTwoTone';
 import './App.css';
 
 const Clock = () => {
 
-  const [break, setBreak] = useState(5)
+  const [brk, setBrk] = useState(5)
+  const [session, setSession] = useState(25)
+  const [chrono, setChrono] = useState(session)
+
+  const Reset = () => {
+    setBrk(5)
+    setSession(25)
+    setChrono(25)
+  }
+
 
   return(
-    <div>
-      <div id="break-label">Break Length
-        <div id="break-length">{break}</div>
-        <button id="break-increment">+</button>
-        <button id="break-decrement">-</button>
+    <div id="container">
+      <div id="row1">
+        <h1>Clock 25 + 5</h1>
+        <div id="parameters">
+          <div id="break-label">Break Length
+            <div id="break-length">{brk}</div>
+            <Button id="break-increment" onClick={() => setBrk(brk + 1)}>
+              <AddBoxTwoToneIcon/>
+            </Button>
+            <Button id="break-decrement" onClick={() => setBrk(brk - 1)}>
+              <IndeterminateCheckBoxTwoToneIcon/>
+            </Button>
+          </div>
+          <div id="session-label">Session Length
+            <div id="session-length">{session}</div>
+            <Button id="session-increment" onClick={() => setSession(session + 1)}>
+              <AddBoxTwoToneIcon/>
+            </Button>
+            <Button id="session-decrement" onClick={() => setSession(session - 1)}>
+              <IndeterminateCheckBoxTwoToneIcon/>
+            </Button>
+          </div>
+        </div>
       </div>
-      <div id="session-label">Session Length
-        <button id="session-increment">+</button>
-        <button id="session-decrement">-</button>
+      
+      <div id="row2">      
+        <div id="timer-label">Session
+          <div id="time-left">{chrono}</div>
+          <Button id="start_stop">
+            <PlayCircleFilledTwoToneIcon/>
+          </Button>
+          <Button id="reset" onClick={() => Reset()}>
+            <ReplayTwoToneIcon/>
+          </Button>
+        </div>
       </div>
+
     </div>
     
   )
